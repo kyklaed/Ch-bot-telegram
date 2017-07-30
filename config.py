8efd43938aed01b4e9dabf497a1cf1d18d2f89ef
Vadim <kyklaed@gmail.com>
2017-07-30 16:08:31 +0300

    no message

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
-token = "374212545428:AAFEuyAqRj6_fdgsdtgtdgfg" #токен
-id_ch = -3243254352234 #id канала
+token = "374212545454353453428:AAFEuyAqRj6_fdgsdtgtdgfg" #токен
+id_ch = -324325435225345334 #id канала
 
